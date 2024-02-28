0ed302aedf9019ef9ef1b3807bfa6e2957d68fb3
Radovan Draskovic <73-2021@pmf.kg.ac.rs>
2024-02-28 22:09:27 +0100

    CRUD app demonstracija upotrebe tehnologija

---
+++ sandbox/pilot_projects/radovan/AngularNETApp/Server/Data/Models/Post.cs
@@ -0,0 +1,8 @@
+﻿namespace Server.Data.Models;
+
+public class Post {
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public bool Published { get; set; }
+}
