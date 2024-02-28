495f2452275d658851c66d061c8b2e076d9125d4
luka.gvozdenovic <lukagvozdenovic02@gmail.com>
2024-02-29 00:48:48 +0100

    Dodat demo projekat

---
+++ sandbox/Luka/ToDo/ToDo/Repository/TodoItemRepository.cs
@@ -0,0 +1,38 @@
+﻿using ToDo.Model;
+
+namespace ToDo.Repository
+{
+    public class TodoItemRepository : ITodoItemRepository
+    {
+        private readonly DataContext context;
+
+        public TodoItemRepository(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public ICollection<TodoItem> GetItems()
+        {
+            return context.TodoItems.ToList();
+        }
+
+        public void Add(TodoItem item)
+        {
+            context.TodoItems.Add(item);
+            context.SaveChanges();
+        }
+
+        public void Delete(int Id)
+        {
+            foreach(TodoItem item in context.TodoItems.ToList())
+            {
+                if(item.Id == Id)
+                {
+                    context.TodoItems.Remove(item);
+                    context.SaveChanges();
+                }
+            }
+
+        }
+    }
+}
