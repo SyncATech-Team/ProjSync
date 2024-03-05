24a0dfb7fb77a31a17e5aaac2e81f3eaf9adedfe
Radovan Draskovic <73-2021@pmf.kg.ac.rs>
2024-03-05 21:34:12 +0100

    Reorganizacija strukture direktorijuma za frontend

--- src/front/angular/src/app/app.module.ts
+++ src/front/angular/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { ContainerLoginComponent } from './login-page/container-login/container-login.component';
+import { LoginPageComponent } from './components/pages/login-page/login-page.component';
+import { ContainerLoginComponent } from './components/elements/container-login/container-login.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
