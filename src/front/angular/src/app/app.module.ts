2cf16e7d7a87091e0fc9fde48a566b809de94413
Mihajlo Nikolic <67-2021@pmf.kg.ac.rs>
2024-03-05 18:32:52 +0100

    Kreirane dve komponente za LogIn #22

--- src/front/angular/src/app/app.module.ts
+++ src/front/angular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ContainerLoginComponent } from './login-page/container-login/container-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    ContainerLoginComponent
   ],
   imports: [
     BrowserModule,
