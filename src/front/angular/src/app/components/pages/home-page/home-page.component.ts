96463f33e6d3608374ec55a9b97885c4c6845f5f
Milan Bajic <milan.bajic98@gmail.com>
2024-03-09 10:54:16 +0100

    #29 zapoceta izrada pocetne strane

--- src/front/angular/src/app/components/pages/home-page/home-page.component.ts
+++ src/front/angular/src/app/components/pages/home-page/home-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../../../_service/account.service';
 import { Router } from '@angular/router';
+import { User } from '../../../_models/user';
+import { Project } from '../../../_models/project.model';
 
 @Component({
   selector: 'app-home-page',
@@ -12,6 +14,75 @@ export class HomePageComponent implements OnInit {
   constructor(public accoutService: AccountService) { }
 
   ngOnInit(): void {
+    
+  }
+  owner: User = {
+    username: "User1",
+    token: ""
+  };
+  project1: Project = {
+    id:0,
+    name: "project1",
+    key: "p1",
+    type: "type1",
+    description: "description1",
+    owner: this.owner,
+    icon: "",
+    createdDate: new Date,
+    dueDate: new Date,
+    budget: 0,
+    visibility: ""
+  }
+  project2: Project = {
+    id:0,
+    name: "project2",
+    key: "p2",
+    type: "type2",
+    description: "description1",
+    owner: this.owner,
+    icon: "",
+    createdDate: new Date,
+    dueDate: new Date,
+    budget: 0,
+    visibility: "",
+    parent: this.project1
+  }
+  project3: Project = {
+    id:0,
+    name: "project3",
+    key: "p3",
+    type: "type3",
+    description: "description3",
+    owner: this.owner,
+    icon: "",
+    createdDate: new Date,
+    dueDate: new Date,
+    budget: 0,
+    visibility: "",
+    parent: this.project1
+  }
+  project4: Project = {
+    id:0,
+    name: "project4",
+    key: "p4",
+    type: "type4",
+    description: "description1",
+    owner: this.owner,
+    icon: "",
+    createdDate: new Date,
+    dueDate: new Date,
+    budget: 0,
+    visibility: "",
+    parent: this.project2
+  }
+  projects: any[] = [
+        this.project1,
+        this.project2,
+        this.project3,
+        this.project4
+  ];
+
+  toProject():void {
     
   }
 }
