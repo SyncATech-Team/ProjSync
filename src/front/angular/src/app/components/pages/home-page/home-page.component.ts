e26a3629c48ded4956ec12b9973736fdfc16090b
Milan Bajic <milan.bajic98@gmail.com>
2024-03-10 23:38:12 +0100

    #29 dodat sort po kolonama u tabeli projekata

--- src/front/angular/src/app/components/pages/home-page/home-page.component.ts
+++ src/front/angular/src/app/components/pages/home-page/home-page.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { User } from '../../../_models/user';
 import { Project } from '../../../_models/project.model';
 import { Observable } from 'rxjs';
+import { Sort,MatSortModule } from '@angular/material/sort';
 
 @Component({
   selector: 'app-home-page',
@@ -18,6 +19,8 @@ export class HomePageComponent implements OnInit {
     this.projects.forEach((project)=>{
       project.subProjects = this.projects.filter((subproject)=>  subproject.parent == project.id );
     });
+
+    this.sortedData=this.projects.slice();
   }
   owner: User = {
     username: "User1",
@@ -26,7 +29,7 @@ export class HomePageComponent implements OnInit {
   project1: any = {
     id:0,
     name: "project1",
-    key: "p1",
+    key: "er",
     type: "Company-managed software",
     description: "description1",
     owner: this.owner,
@@ -41,7 +44,7 @@ export class HomePageComponent implements OnInit {
   project2: any = {
     id:1,
     name: "project2",
-    key: "p2",
+    key: "ty",
     type: "Company-managed software",
     description: "description1",
     owner: this.owner,
@@ -57,7 +60,7 @@ export class HomePageComponent implements OnInit {
   project3: any = {
     id:2,
     name: "project3",
-    key: "p3",
+    key: "ps",
     type: "Company-managed software",
     description: "description3",
     owner: this.owner,
@@ -73,7 +76,7 @@ export class HomePageComponent implements OnInit {
   project4: any = {
     id:3,
     name: "project4",
-    key: "p4",
+    key: "pt",
     type: "Company-managed software",
     description: "description1",
     owner: this.owner,
@@ -92,12 +95,43 @@ export class HomePageComponent implements OnInit {
         this.project3,
         this.project4
   ];
-
+  sortedData: any[]=[];
+  
   toProject():void {
     
   }
 
   toggleRow(project: any): void{
     project.isExtanded = !project.isExtanded;
+  }
+
+  sortData(sort: Sort) {
+    const data = this.projects.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedData = data;
+      return;
+    }
+
+    this.sortedData = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'name':
+          return this.compare(a.name, b.name, isAsc);
+        case 'key':
+          return this.compare(a.key, b.key, isAsc);
+        case 'type':
+          return this.compare(a.type, b.type, isAsc);
+        case 'lead':
+          return this.compare(a.owner.username, b.owner.username, isAsc);
+        case 'stared':
+          return this.compare(Number(a.isFavorite), Number(b.isFavorite), isAsc);
+        default:
+          return 0;
+      }
+    });
+  }
+
+  compare(a: number | string, b: number | string, isAsc: boolean) {
+    return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
   }
 }
