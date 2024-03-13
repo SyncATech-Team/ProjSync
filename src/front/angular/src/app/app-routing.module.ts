d7ec831821986ac3f4a398835f1b9cc4d3b155c9
Mihajlo Nikolic <67-2021@pmf.kg.ac.rs>
2024-03-13 20:52:19 +0100

    Izmena sidenavBar komponente, promenjena struktura admin stranice, verzija 1 #39

--- src/front/angular/src/app/app-routing.module.ts
+++ src/front/angular/src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './components/pages/login-page/login-page.component';
 import { HomePageComponent } from './components/pages/home-page/home-page.component';
 import { AdminPageComponent } from './components/pages/admin-page/admin-page.component';
+import { DashboardPageComponent } from './components/pages/admin-page/dashboard-page/dashboard-page.component';
+import { UserPageComponent } from './components/pages/admin-page/user-page/user-page.component';
 
 const routes: Routes = [
   {path: '', component: LoginPageComponent},
   {path: 'home', component: HomePageComponent},
   {path: 'admin', component: AdminPageComponent},
-  {path: '**', component: LoginPageComponent, pathMatch: 'full'}
+  {path: 'admin/dashboard', component: DashboardPageComponent},
+  {path: 'admin/user', component: UserPageComponent},
+  {path: '**', component: LoginPageComponent, pathMatch: 'full'},
 ];
 
 @NgModule({
---
+++ src/front/angular/src/app/components/elements/admin-sidebar/nav-data.ts
@@ -0,0 +1,12 @@
+export const navbarData = [
+    {
+        routeLink : '/admin/dashboard',
+        icon: 'fa-solid fa-chart-line',
+        label: 'Dashboard'
+    },
+    {
+        routeLink : '/admin/user',
+        icon: 'fa-solid fa-user',
+        label: 'Users'
+    }
+];
