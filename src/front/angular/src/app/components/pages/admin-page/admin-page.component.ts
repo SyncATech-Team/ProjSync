48cbea6eeb2cab00199f165bd56e3a019bf0de73
Radovan Draskovic <73-2021@pmf.kg.ac.rs>
2024-03-15 20:21:53 +0100

    #47 - Dodate default ikonice naloga; Kreirane izmene u prikazu tabela

--- src/front/angular/src/app/components/pages/admin-page/admin-page.component.ts
+++ src/front/angular/src/app/components/pages/admin-page/admin-page.component.ts
@@ -33,35 +33,51 @@ export class AdminPageComponent {
     this.visibleRole = this.visibleRole?false:true;
   }
 
-  close_alert(getByid: string) {
-
-    let x = document.getElementById(getByid);
-    if(x != null) {
-      x.hidden = true;
-    }
-  }
-  
-  close_all_alerts() {
-    this.close_alert("invalid_register_div");
-    this.close_alert("invalid_role_div");
-    this.close_alert("valid_register_div");
-    this.close_alert("valid_role_div");
-  }
-
-  hide_all_pages() {
-
-    let x = document.getElementById("welcome");
-    if(x != null) x.hidden = true;
-
-    let y = document.getElementById("users");
-    if(y != null) y.hidden = true;
-
-    let z = document.getElementById("company_roles");
-    if(z != null) z.hidden = true;
-  }
-
   onToggleSideNav(data: SideNavToggle) {
     this.screenWidth = data.screenWidth;
     this.isSideNavCollapsed = data.collapsed;
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// close_alert(getByid: string) {
+
+//   let x = document.getElementById(getByid);
+//   if(x != null) {
+//     x.hidden = true;
+//   }
+// }
+
+// close_all_alerts() {
+//   this.close_alert("invalid_register_div");
+//   this.close_alert("invalid_role_div");
+//   this.close_alert("valid_register_div");
+//   this.close_alert("valid_role_div");
+// }
+
+// hide_all_pages() {
+
+//   let x = document.getElementById("welcome");
+//   if(x != null) x.hidden = true;
+
+//   let y = document.getElementById("users");
+//   if(y != null) y.hidden = true;
+
+//   let z = document.getElementById("company_roles");
+//   if(z != null) z.hidden = true;
+// }
