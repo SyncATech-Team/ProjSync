2695921b25341938e0c64075dadd44eda1763735
Mihajlo Nikolic <67-2021@pmf.kg.ac.rs>
2024-03-16 01:52:49 +0100

    Napravljen entitet back, notification dto i repository, zapocet kontroler #48

---
+++ src/back/backAPI/backAPI/Entities/Domain/Notification.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace backAPI.Entities.Domain
+{
+    [Table("Notifications")]
+    public class Notification
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, NotNull]
+        public int User_Id { get; set; }
+
+        [Required, NotNull]
+        public string Message { get; set; }
+
+        [Required, NotNull]
+        public DateTime DateCreated { get; set; }
+    }
+}
---
+++ src/back/backAPI/backAPI/Repositories/Interface/INotificationsRepository.cs
@@ -0,0 +1,22 @@
+﻿using backAPI.Entities.Domain;
+
+namespace backAPI.Repositories.Interface
+{
+    public interface INotificationsRepository
+    {
+        //DOVLACENJE SVIH NOTIFIKACIJA
+        Task<IEnumerable<Notification>> GetAllNotificationsAsync();
+
+        //DOVLACENJE NOTIFIKACIJE PO ID 
+        Task<Notification> GetNotificationByIdAsync(int id);
+
+        //DOVLACENJE NOTIFIKACIJE PO ID KORISNIKA
+        Task<Notification> GetNotificationByUserIdAsync(int userId);
+
+        //DODAVANJE NOVE NOTIFIKACIJE
+        Task<Notification> AddNotificationAsync(Notification notification);
+
+        //BRISANJE NOTIFIKACIJE PO ID
+        Task<Boolean> DeleteNotificationAsync(int id);
+    }
+}
