168ed9a57cfeef09a52184730ab501c77343b09a
VGeroski <vlada.geroski@gmail.com>
2024-03-17 19:51:04 +0100

    izbacivanje nepotrebnih komponenti

--- src/front/angular/src/app/components/elements/create-role/create-role.component.ts
+++ src/front/angular/src/app/components/elements/create-role/create-role.component.ts
@@ -1,9 +1,5 @@
 import { Component, Output, EventEmitter } from '@angular/core';
-import { AccountService } from '../../../_service/account.service';
-import { AdminPageComponent } from '../../pages/admin-page/admin-page.component';
-import { CompanyroleService } from '../../../_service/companyrole.service';
 import { CompanyRole } from '../../../_models/company-role';
-import { RolePageComponent } from '../../pages/admin-page/role-page/role-page.component';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
