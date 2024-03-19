2979fc858893dc81c8f4bb60f99023cf651a57c6
VGeroski <vlada.geroski@gmail.com>
2024-03-19 12:16:58 +0100

    izbacivanje nepotrebih import-a

--- src/front/angular/src/app/_guards/admin.guard.ts
+++ src/front/angular/src/app/_guards/admin.guard.ts
@@ -1,7 +1,6 @@
 import { CanActivateFn } from '@angular/router';
 import { AccountService } from '../_service/account.service';
 import { inject } from '@angular/core';
-import { map } from 'rxjs';
 
 export const adminGuard: CanActivateFn = (route, state) => {
   const accountService = inject(AccountService);
--- src/front/angular/src/app/_guards/auth.guard.ts
+++ src/front/angular/src/app/_guards/auth.guard.ts
@@ -1,7 +1,6 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AccountService } from '../_service/account.service';
-import { map } from 'rxjs';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const accountService = inject(AccountService);
