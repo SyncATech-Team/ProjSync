44fea6b6d2caae6602da9d4cef91bcc0fcbfd59d
Radovan Draskovic <73-2021@pmf.kg.ac.rs>
2024-03-23 23:44:17 +0100

    Dodata ruta ka stranici projekta koja sadrzi naziv projekta

--- src/front/angular/src/app/app-routing.module.ts
+++ src/front/angular/src/app/app-routing.module.ts
@@ -23,7 +23,8 @@ const routes: Routes = [
       {path: 'admin/dashboard', component: DashboardPageComponent, canActivate: [adminGuard]},
       {path: 'admin/user', component: UserPageComponent, canActivate: [adminGuard]},
       {path: 'admin/role', component: RolePageComponent, canActivate: [adminGuard]},
-      {path: 'home/projects', component: ProjectPageComponent}
+      {path: 'home/projects', component: ProjectPageComponent},
+      {path: 'home/projects/:projectName', component: ProjectPageComponent}
     ]
   },
   {path: '**', component: LoginPageComponent, pathMatch: 'full'},
--- src/front/angular/src/app/components/pages/project-page/project-page.component.ts
+++ src/front/angular/src/app/components/pages/project-page/project-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-project-page',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './project-page.component.css'
 })
 export class ProjectPageComponent {
+  
 
+  constructor(private route: ActivatedRoute) {
+    console.log("Constructor " + this.route.snapshot.paramMap.get('projectName'));
+  }
 }
