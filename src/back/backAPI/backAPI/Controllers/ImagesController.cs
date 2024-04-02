c05f825ae8045adb069bc873c25f2cda15aa8b54
Mihajlo Nikolic <67-2021@pmf.kg.ac.rs>
2024-04-02 15:03:03 +0200

    Inicijalna izrada upload image za korisnika -#88

---
+++ src/back/backAPI/backAPI/Controllers/ImagesController.cs
@@ -0,0 +1,51 @@
+﻿using backAPI.Repositories.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backAPI.Controllers
+{
+    public class ImagesController : BaseApiController
+    {
+        private readonly IImageRepository _imageRepository;
+
+        public ImagesController(IImageRepository imageRepository)
+        {
+            _imageRepository = imageRepository;
+        }
+
+        [HttpPost("user/{username}")]
+        public async Task<ActionResult> UploadUserImage(string username, IFormFile imageFile)
+        {
+            if (imageFile == null || imageFile.Length == 0)
+            {
+                return BadRequest("Invalid image file");
+            }
+
+            var imageUrl = await _imageRepository.UploadUserImage(username, imageFile);
+
+            if (imageUrl == null)
+            {
+                return BadRequest("Failed to upload image");
+            }
+
+            return Ok(new { ImageUrl = imageUrl });
+        }
+
+        [HttpPost("other")]
+        public async Task<ActionResult> UploadOtherImage(IFormFile imageFile)
+        {
+            if (imageFile == null || imageFile.Length == 0)
+            {
+                return BadRequest("Invalid image file");
+            }
+
+            var imageUrl = await _imageRepository.UploadOtherImage(imageFile);
+
+            if (imageUrl == null)
+            {
+                return BadRequest("Failed to upload image");
+            }
+
+            return Ok(new { ImageUrl = imageUrl });
+        }
+    }
+}
---
+++ src/back/backAPI/backAPI/Repositories/Implementation/ImageRepository.cs
@@ -0,0 +1,71 @@
+﻿using backAPI.Repositories.Interface;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace backAPI.Repositories.Implementation
+{
+    public class ImageRepository : IImageRepository
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ImageRepository(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public async Task<string> UploadUserImage(string username, IFormFile imageFile)
+        {
+            if (string.IsNullOrEmpty(username))
+            {
+                return null;
+            }
+
+            if (imageFile == null || imageFile.Length == 0)
+            {
+                return null;
+            }
+
+            // JEDINSTVENO IME ZA FAJL
+            var uniqueFileName = Guid.NewGuid().ToString() + "_" + imageFile.FileName;
+
+            // PUTANJA DO USER-IMAGES FOLDERA
+            var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "..\\user-images");
+
+            if (!Directory.Exists(uploadsFolder))
+            {
+                Directory.CreateDirectory(uploadsFolder);
+            }
+
+            // PUTANJA DO KONACNOG FAJLA
+            var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+            //CUVAM SLIKU OVDE
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                await imageFile.CopyToAsync(fileStream);
+            }
+
+            return "/user-images/" + uniqueFileName;
+        }
+
+
+        public async Task<string> UploadOtherImage(IFormFile imageFile)
+        {
+            var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "user-images");
+
+            if (!Directory.Exists(uploadsFolder))
+            {
+                Directory.CreateDirectory(uploadsFolder);
+            }
+
+            var uniqueFileName = Guid.NewGuid().ToString() + "_" + imageFile.FileName;
+            var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                await imageFile.CopyToAsync(fileStream);
+            }
+
+            return "/user-images/" + uniqueFileName;
+        }
+    }
+}
