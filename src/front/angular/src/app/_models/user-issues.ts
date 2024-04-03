34ca19c5e7c9f72c2a146c4626766ad9a6cfa864
VGeroski <vlada.geroski@gmail.com>
2024-04-03 13:24:40 +0200

    dodavanje potrebnih modela

---
+++ src/front/angular/src/app/_models/user-issues.ts
@@ -0,0 +1,17 @@
+export interface JUser {
+    username: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    companyRoleName: string;
+    profilePhoto?: string;
+    address: string;
+    contactPhone: string;
+    status: string;
+    isVerified: boolean;
+    preferedLanguage: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+    isActive?: boolean;
+    issueIds: string[];
+}
