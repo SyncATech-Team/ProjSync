4e9ccf86b660a1c972e4cb75098811a0819b4dd2
VGeroski <vlada.geroski@gmail.com>
2024-04-08 18:44:16 +0200

    reporter komponenta - povezana i funkcionalna

--- src/front/angular/src/app/components/elements/issues/issue-reporter/issue-reporter.component.ts
+++ src/front/angular/src/app/components/elements/issues/issue-reporter/issue-reporter.component.ts
@@ -1,19 +1,31 @@
-import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
-import {JIssue} from "../../../../_models/issue";
-import {JUser} from "../../../../_models/user-issues";
-import {ProjectService} from "../../../state/project/project.service";
+import {
+  ChangeDetectorRef,
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges
+} from '@angular/core';
+import { JIssue } from "../../../../_models/issue";
+import { JUser } from "../../../../_models/user-issues";
+import { ProjectService } from "../../../state/project/project.service";
+import { OverlayPanel } from "primeng/overlaypanel";
 
 @Component({
   selector: 'issue-reporter',
   templateUrl: './issue-reporter.component.html',
   styleUrl: './issue-reporter.component.css'
 })
-export class IssueReporterComponent implements OnChanges {
+export class IssueReporterComponent implements OnInit, OnChanges {
   @Input() issue!: JIssue;
   @Input() users!: JUser[] | null;
   reporter: JUser | undefined;
 
-  constructor(private _projectService: ProjectService) {}
+  constructor(private _projectService: ProjectService, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    this.cdr.markForCheck();
+  }
 
   ngOnChanges(changes: SimpleChanges) {
     const issueChange = changes['issue'];
@@ -26,10 +38,11 @@ export class IssueReporterComponent implements OnChanges {
     return user.id === this.issue.reporterId;
   }
 
-  updateIssue(user: JUser) {
+  updateIssue(user: JUser, op: OverlayPanel) {
     this._projectService.updateIssue({
       ...this.issue,
       reporterId: user.id
     });
+    op.hide();
   }
 }
