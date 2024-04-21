6abc40fb3b46beffc5c482b6857166fad4a76b54
VGeroski <vlada.geroski@gmail.com>
2024-04-21 21:56:42 +0200

    dodavanje profilnih slika na filter board komponentu

--- src/front/angular/src/app/components/elements/board/board-filter/board-filter.component.ts
+++ src/front/angular/src/app/components/elements/board/board-filter/board-filter.component.ts
@@ -7,6 +7,8 @@ import {FilterService} from "../../../state/filter/filter.service";
 import {debounceTime, distinctUntilChanged} from "rxjs";
 import {JUser} from "../../../../_models/user-issues";
 import {AccountService} from "../../../../_service/account.service";
+import {PhotoForUser} from "../../../../_models/photo-for-user";
+import {UserProfilePicture} from "../../../../_service/userProfilePicture.service";
 
 @Component({
   selector: 'board-filter',
@@ -17,12 +19,15 @@ import {AccountService} from "../../../../_service/account.service";
 export class BoardFilterComponent implements OnInit {
   searchControl: FormControl = new FormControl('');
   userIds: string[];
+  users!: JUser[];
+  usersPhotos: PhotoForUser[] = [];
 
   constructor(
     public projectQuery: ProjectQuery,
     public filterQuery: FilterQuery,
     public filterService: FilterService,
-    private accountService: AccountService
+    private accountService: AccountService,
+    private userPictureService: UserProfilePicture,
   ) {
     this.userIds = [];
   }
@@ -37,6 +42,10 @@ export class BoardFilterComponent implements OnInit {
     this.filterQuery.userIds$.pipe(untilDestroyed(this)).subscribe((userIds) => {
       this.userIds = userIds;
     });
+    this.projectQuery.users$.pipe(untilDestroyed(this)).subscribe((users) => {
+      this.users = users;
+    })
+    this.getUserProfilePhotos(this.users);
   }
 
   isUserSelected(user: JUser) {
@@ -60,5 +69,47 @@ export class BoardFilterComponent implements OnInit {
   resetAll() {
     this.searchControl.setValue('');
     this.filterService.resetAll();
+  }
+
+  getUserProfilePhotos(users: JUser[] | null) {
+    if (!users) return;
+    for(const user of users) {
+      if(user.profilePhoto != null) {
+        this.userPictureService.getUserImage(user.username).subscribe({
+          next: response => {
+            var path = this.userPictureService.decodeBase64Image(response['fileContents']);
+            var ph: PhotoForUser = {
+              username: user.username,
+              photoSource: path
+            };
+            this.usersPhotos.push(ph);
+          },
+          error: error => {
+            console.log(error);
+          }
+        });
+      }
+      else {
+        var ph: PhotoForUser = {
+          username: user.username,
+          photoSource: "SLIKA_JE_NULL"
+        }
+        this.usersPhotos.push(ph);
+      }
+    }
+  }
+
+  UserImagePath(username: string | undefined): string {
+    if (!this.users) return "";
+    let index = this.users.findIndex(u => u.username === username);
+    if(index == -1) return this.userPictureService.getFirstDefaultImagePath();
+
+    if(this.users[index].profilePhoto == null)
+      return this.userPictureService.getDefaultImageForUser(this.users[index].username);
+
+    let ind = this.usersPhotos.findIndex(u => u.username == username);
+    if(ind == -1) return this.userPictureService.getFirstDefaultImagePath();
+    console.log('[userPhotoSource]', this.usersPhotos[ind].photoSource);
+    return this.usersPhotos[ind].photoSource;
   }
 }
