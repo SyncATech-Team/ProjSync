2f378079ee01670f4e82ff6b8d65ded2875efc68
VGeroski <vlada.geroski@gmail.com>
2024-05-14 22:21:29 +0200

    dodavanje komponente za unos mejla

--- src/front/angular/src/app/components/elements/forgot-password/forgot-password.component.ts
+++ src/front/angular/src/app/components/elements/forgot-password/forgot-password.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { EmailValidationService } from '../../../_service/email_validator.service';
+import { Router } from '@angular/router';
+import { AccountService } from '../../../_service/account.service';
+import { MessageService } from "primeng/api";
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrl: './forgot-password.component.css'
 })
 export class ForgotPasswordComponent {
+  user : any = {
+    email:  "",
+    password:  ""
+  }
 
+  emailValid: boolean = false;
+
+  constructor(
+    public accountService: AccountService,
+    private router: Router,
+    private mailValidationService: EmailValidationService,
+    private messageService: MessageService
+  ) { }
+
+  validateEmail(email: string): boolean {
+    this.emailValid = this.mailValidationService.isValidEmailAddress(email);
+    return this.emailValid;
+  }
+
+  forgot_pass() {
+    if (!this.emailValid) {
+      return;
+    }
+  }
 }
