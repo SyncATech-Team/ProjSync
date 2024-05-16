545b19ad237eb2e6d846e1010b9864a74296ad5b
Radovan Draskovic <73-2021@pmf.kg.ac.rs>
2024-05-16 22:24:51 +0200

    Nova migracija; sinhronizacija baze

--- src/back/backAPI/backAPI/Migrations/20240516202423_PostProductionMigrationFix.cs
+++ src/back/backAPI/backAPI/Migrations/20240516202423_PostProductionMigrationFix.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace backAPI.Migrations
 {
     /// <inheritdoc />
-    public partial class MigrationIssueDocumentation : Migration
+    public partial class PostProductionMigrationFix : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -373,6 +373,27 @@ namespace backAPI.Migrations
                 });
 
             migrationBuilder.CreateTable(
+                name: "Logs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ProjectId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Message = table.Column<string>(type: "TEXT", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Logs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Logs_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
                 name: "ProjectDocumentations",
                 columns: table => new
                 {
@@ -577,7 +598,7 @@ namespace backAPI.Migrations
                         column: x => x.IssueId,
                         principalTable: "Issues",
                         principalColumn: "Id",
-                        onDelete: ReferentialAction.Restrict);
+                        onDelete: ReferentialAction.Cascade);
                 });
 
             migrationBuilder.CreateIndex(
@@ -701,6 +722,11 @@ namespace backAPI.Migrations
                 unique: true);
 
             migrationBuilder.CreateIndex(
+                name: "IX_Logs_ProjectId",
+                table: "Logs",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
                 name: "IX_Notifications_UserId",
                 table: "Notifications",
                 column: "UserId");
@@ -801,6 +827,9 @@ namespace backAPI.Migrations
 
             migrationBuilder.DropTable(
                 name: "IssueDocumentation");
+
+            migrationBuilder.DropTable(
+                name: "Logs");
 
             migrationBuilder.DropTable(
                 name: "Notifications");
