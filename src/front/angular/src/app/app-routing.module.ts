cfd55bf8ea82c8dcd12fa131c1acaef328034655
VGeroski <vlada.geroski@gmail.com>
2024-05-21 21:22:24 +0200

    brisanje nepotrebnih komponenti

--- src/front/angular/src/app/app-routing.module.ts
+++ src/front/angular/src/app/app-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './components/pages/login-page/login-page.component';
 import { HomePageComponent } from './components/pages/home-page/home-page.component';
-import { AdminPageComponent } from './components/pages/admin-page/admin-page.component';
-import { DashboardPageComponent } from './components/pages/admin-page/dashboard-page/dashboard-page.component';
 import { UserPageComponent } from './components/pages/admin-page/user-page/user-page.component';
 import { RolePageComponent } from './components/pages/admin-page/role-page/role-page.component';
 import { PasswordResetPageComponent } from './components/pages/password-reset-page/password-reset-page.component';
